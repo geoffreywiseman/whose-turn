77d9e663f0969af77768d9f6c46190bbc70b7ef7
Geoffrey Wiseman <geoffrey.wiseman@gmail.com>
2014-09-16 22:26:23 -0400

    MongoDB Persistence
    Using MongoDB as the persistent datastore for Whose Turn. Works locally, so now it's time to see if it's working on Heroku.
    
    Part of #1, but I'm not closing it until I try on Heroku.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,28 +4,38 @@
 	var whoseTurn = angular.module('whoseTurnApp', [ 'ngRoute', 'ngResource' ]);
 
 	// Controllers
-	whoseTurn.controller( 'ListCtrl', ['$scope', '$resource', function($scope, $resource) {
-		var List = $resource( '/api/list/:listName' );
+	whoseTurn.controller( 'ListCtrl', ['$scope', '$resource', '$routeParams', function($scope, $resource, $routeParams) {
+		var List = $resource( '/api/lists/:listId' );
 		$scope.listRetrieved = null;
-		$scope.memberList = List.get( { listName: 'jedi' }, function( ) {
+		console.log( "Requesting list: " + $routeParams.listId );
+		$scope.memberList = List.get( { listId: $routeParams.listId }, function( ) {
 			console.log( "List retrieved: " + $scope.memberList.name );
 			$scope.listRetrieved = true;
 		}, function( ) {
 			$scope.listRetrieved = false;
 		} );
-	}] );
+	} ] );
+
+	whoseTurn.controller( 'ListsCtrl', ['$scope', '$resource', function( $scope, $resource, $routeParams ) {
+		var List = $resource( '/api/lists/:listId' );
+		$scope.lists = List.query( );
+	} ] );
 
 
 	// Routes
 	whoseTurn.config( [ '$routeProvider', function($router) {
-			$router.when( '/list', {
+			$router.when( '/', {
+				templateUrl: 'views/lists.html',
+				controller: 'ListsCtrl'
+			} );
+			$router.when( '/lists/:listId', {
 					templateUrl: 'views/list.html',
 					controller: 'ListCtrl'
 				} );
 			$router.when( '/about', {
 					templateUrl: 'views/about.html'
 				} );
-			$router.otherwise( { redirectTo: '/list' } );
+			$router.otherwise( { redirectTo: '/' } );
 		} ] );
 
 	// Directives
--- server/web-server.js
+++ server/web-server.js
@@ -1,61 +1,108 @@
+"use strict";
+
+// dependencies
 var express = require( 'express' );
 var app = express( );
 var path = require( 'path' );
+var mongo = require( 'mongodb' );
 
-// environment
+// globals
 var env = process.env.NODE_ENV || 'development';
-var static_dir;
-if( 'development' == env ) {
-	static_dir = '../client';
-} else {
-	static_dir = '../dist';
-}
-static_dir = path.normalize( __dirname + '/' + static_dir );
-console.log( "Serving static files from: " + static_dir );
+var db;
 
-// logging
-var logfmt = require( 'logfmt' );
-app.use( logfmt.requestLogger() );
-
-// static files
-app.use( express.static( static_dir ) );
-
-// fake data
-var lists = {
-	cppLunchWheel: {
-		name: 'CPP Lunch Wheel',
-		members: [
-			{ name: 'Douglas', score: 3 },
-			{ name: 'Paul', score: -1 },
-			{ name: 'Anita', score: 3 },
-			{ name: 'Tony', score: -2 }
-		]
-	},
-	jedi: {
-		name: 'Jedi Knights',
-		members: [
-			{ name: 'Luke', score: 50 },
-			{  name: 'Yoda', score: -100 },
-			{ name: 'Obi-Wan', score: -25 }
-		]
+//  setup
+function setupStaticFiles( ) {
+	var static_dir;
+	if( 'development' == env ) {
+		static_dir = '../client';
+	} else {
+		static_dir = '../dist';
 	}
-};
+	static_dir = path.normalize( __dirname + '/' + static_dir );
+	console.log( "Serving static files from: " + static_dir );
+	app.use( express.static( static_dir ) );
+}
+
+function setupLogging() {
+	var logfmt = require( 'logfmt' );
+	app.use( logfmt.requestLogger() );
+}
+
+setupLogging();
+setupStaticFiles();
+
+// startup
+function startup() {
+	var port = Number( process.env.PORT || 5000 );
+	var mongoUrl = process.env.MONGOHQ_URL;
+
+	if( !mongoUrl ) {
+		throw "No persistent datastore";
+	}
+
+	mongo.MongoClient.connect( mongoUrl, function( err, connection ) {
+		if( err ) {
+			console.log( "Couldn't connect to MongoDB." );
+			throw err;
+		} else {
+			db = connection;
+			console.log( "Connection established to MongoDB." );
+			app.listen( port, function() {
+  				console.log( "Express (" + env + ") is listening on port: " + port );
+			} );
+		}
+	} );			
+}
 
 // api
-app.get( '/api/list/:listName', function( req, res ) {
-	var listName = req.params.listName;
-	if( lists[ listName ] === undefined ) {
-		console.log( "Requested unknown list: " + listName );
-		res.status( 404 ).send( );
-	}
-	else {
-		console.log( "Returning requested list: " + listName );
-		res.status( 200 ).send( lists[listName] );
-	}
+app.get( '/api/lists', function( req, res ) {
+	console.log( "Retrieving all lists." );
+	db.collection( 'lists', { strict: true }, function( err, collection ) {
+		if( err ) {
+			console.log( "No 'lists' collection in datastore." );
+			console.dir( err );
+			res.status( 500 ).send( "Could not retrieve lists from persistent datastore." );
+		} else {
+			collection.find( {}, { _id: 1, name: 1 } ).toArray( function( err, listOfLists ) {
+				if( err ) {
+					console.error( "Could not retrieve list of lists." );
+					console.dir( err );
+				} else {
+					console.log( "Returning list of lists." );
+					res.status( 200 ).send( listOfLists );
+				}
+			} );
+		}
+	} );
 } );
 
-// server
-var port = Number( process.env.PORT || 5000 );
-app.listen( port, function() {
-  console.log( "Express (" + env + ") is listening on port: " + port );
+app.get( '/api/lists/:listId', function( req, res ) {
+	var listId = req.params.listId;
+	console.log( "Attempting to retrieve list for id: " + listId );
+	db.collection( 'lists', { strict: true }, function( err, lists ) {
+		if( err ) {
+			console.log( "Couldn't find 'lists' collection." );
+			console.dir( err );
+			res.status( 500 ).send( "Couldn't retrieve lists from persistent datastore." );
+		} else {
+			lists.findOne( { "_id": mongo.ObjectID( listId ) }, function( err, list ) {
+				if( err ) {
+					console.log( "Couldn't retrieve list:" );
+					console.dir( err );
+					res.status( 500 ).send( "Couldn't retrieve list." );
+				} else {
+					if( list ) {
+						console.log( "Returning requested list: " + listId );
+						res.status( 200 ).send( list );
+					} else {
+						console.log( "Requested unknown list: " + listId );
+						res.status( 404 ).send( );
+					}
+				}
+			} );
+		}
+	} );
 } );
+
+
+startup();
